a8ad05605197b09e550a8ef6d534bde5d9299ab7
Johannes Goller <jogojapan@gmail.com>
2012-11-24 23:18:30 +0900

    Adding tuple tools.

---
+++ src/util/tupletools.hpp
@@ -0,0 +1,128 @@
+/*
+ * tupletools.hpp
+ *
+ *  Created on: 2012/11/24
+ *      Author: gollerjo
+ */
+
+#ifndef TUPLETOOLS_HPP_
+#define TUPLETOOLS_HPP_
+
+#include <tuple>
+
+namespace rlxutil {
+namespace tuple {
+
+template <unsigned... Is>
+struct indices
+{ };
+
+/**
+ * Use this is you need a variadic template argument consisting
+ * of N unsigned integers `N-1,N-2,...0`. Example: Given a variadic
+ * function template with unsigned integer parameters:
+ *
+ *     template <unsigned... Is>
+ *     void FunTemplate(indices<Is...>)
+ *     {
+ *       // Now you can use Is... in variadic template arguments. //
+ *     };
+ *
+ * You can call the template function using the `index_maker`:
+ *
+ *     // Call the template function above with <4,3,2,1,0>:
+ *     FunTemplate(typename index_maker<5>::type());
+ */
+template <unsigned N, unsigned... Is>
+struct index_maker : index_maker<N-1,N-1,Is...>
+{ };
+
+template <unsigned... Is>
+struct index_maker<0,Is...>
+{ typedef indices<Is...> type; };
+
+
+/**
+ * Helper function for the tuple-map.
+ */
+template <typename F, typename... Ts, unsigned... Is>
+std::tuple<Ts...> map_(
+    F f,
+    const std::tuple<Ts...> &tup,
+    rlxutil::tuple::indices<Is...>)
+{
+  return std::make_tuple(f(std::get<Is>(tup))...);
+}
+
+/**
+ * Tuple-map, i.e. a template that applies a function to
+ * all elements of a tuple and returns a new tuple of the
+ * same length that contains the results.
+ *
+ * Example:
+ *
+ *     inline constexpr int half(int i) {
+ *        return i / 2;
+ *     }
+ *
+ *     std::tuple<int,int,int> tup { 2,4,8 };
+ *     auto ntup = rlxutil::tuple::map(half,tup);
+ *     rlxutil::tuple::map([](int i){std::cout << i; return 0;},ntup);
+ *     std::cout << std::endl;
+ *
+ */
+template <typename F, typename... Ts>
+std::tuple<Ts...> map(
+    F f,
+    const std::tuple<Ts...> &tup)
+{
+  return map_(f,tup,typename index_maker<sizeof...(Ts)>::type());
+}
+
+/**
+ * Helper function for `template_map`.
+ */
+template <template <class> class F, typename... Ts, unsigned... Is>
+std::tuple<Ts...> template_map_(
+    const std::tuple<Ts...> &tup,
+    rlxutil::tuple::indices<Is...>)
+{
+  return std::make_tuple(F<Ts>::apply(std::get<Is>(tup))...);
+}
+
+/**
+ * Tuple-map using a type-dependent function, i.e. a template
+ * that applies a function template to all elements of a tuple
+ * and returns a new tuple of the same length that contains
+ * the results. The function template must be defined as a
+ * static member function of a struct/class template. The
+ * prototype of the function must be `apply(T t)`.
+ *
+ * Example:
+ *
+ *     template <typename T>
+ *     struct template_half
+ *     {
+ *       static constexpr T apply(T t)
+ *       { return t / 4; }
+ *     };
+ *
+ *     std::tuple<float,int,int> tup { 2.0,4,8 };
+ *     auto ntup = rlxutil::tuple::template_map<template_half>(tup);
+ *     std::cout << std::get<0>(ntup) << ','
+ *               << std::get<1>(ntup) << ','
+ *               << std::get<2>(ntup) << std::endl;
+ *
+ */
+template <template <class> class F, typename... Ts>
+std::tuple<Ts...> template_map(
+    const std::tuple<Ts...> &tup)
+{
+  return template_map_<F>(tup,typename index_maker<sizeof...(Ts)>::type());
+}
+
+} // namespace tuple
+} // namespace rlxutil
+
+
+#endif /* TUPLETOOLS_HPP_ */
